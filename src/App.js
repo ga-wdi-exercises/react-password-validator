e89435a58c9d703cd674a0931c3d40facf05337d
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-11-14 15:23:08 -0500

    created form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Sign In </h1>
+        <form onSubmit={this.handleSubmit}>
+        Name:
+        <input type="text"/>
+        Password:
+        <input type="password"/>
+        Confirm Password:
+        <input type="password"/>
+        <button onClick= {(e)=>{this.checkPassword(e)}}>Submit</button>
+        </form>
       </div>
     );
   }
