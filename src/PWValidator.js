3c202cbc469bbd356424ae978ace4bcb7fd793c5
Michael Lai <mike24567@gmail.com>
2016-11-14 16:38:53 -0500

    finished password validation

---
+++ src/PWValidator.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+class PWValidator extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      email: "",
+      pass: "",
+      confirmPass: ""
+    }
+    this.handleEmail = this.handleEmail.bind(this)
+    this.handlePass = this.handlePass.bind(this)
+    this.handleConfirmPass = this.handleConfirmPass.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  //sets the new email
+  handleEmail(e) {
+    this.setState({
+      email: e.target.value,
+    })
+  }
+  //sets the new pass
+  handlePass(e) {
+    this.setState({
+      pass: e.target.value,
+    })
+  }
+  //confirms new pass
+  handleConfirmPass(e) {
+    console.log(e.target.value)
+    this.setState({
+      confirmPass: e.target.value,
+    })
+  }
+  //confirms the submission
+  handleSubmit(e) {
+    console.log("consequences...")
+    if (this.state.pass === this.state.confirmPass){
+      alert("congrats hoe")
+    } else{
+      alert("try again hoe")
+    }
+
+    e.preventDefault()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Password Validator</h1>
+        <form onSubmit={this.handleSubmit}>
+         <p>{this.state.email}</p>
+          <p>Email
+            <input type="text" value={this.state.email} onChange={this.handleEmail} /></p>
+          <p>Password
+            <input type="text" value={this.state.pass} onChange={this.handlePass} /></p>
+          <p>Confirm Password
+            <input type="text" value={this.state.confirmPass} onChange={this.handleConfirmPass} /></p>
+          <p><input type="submit" value="Submit" /></p>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PWValidator;
