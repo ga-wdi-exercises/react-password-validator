439c16843cc71aa1a28c7e0fa31ae4c828e8de08
Khoi Le <khoileee89@gmail.com>
2016-11-14 16:34:04 -0500

    view renders

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+
+class Validator extends Component {
+  constructor(props){
+    super()
+    this.state = {
+    }
+  }
+  checkPassword (e) {
+
+    this.setState({
+
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div>
+          <form>
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Password" />
+            <input type="text" placeholder="Password Confirmation" />
+          </form>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
 
-export default App;
+export default Validator;
+//forms dont need to change, just need to check the previous value with the new value.
