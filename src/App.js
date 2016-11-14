f24bc136f4e9fe757ad6dcb1df78053d7900aaab
manish pradhan <pradhanmanish33@gmail.com>
2016-11-14 16:18:21 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let options = {
+      email: {
+        label: "Email Address",
+        placeholder: "Email"
+      },
+      password: {
+        label: "Password",
+        placeholder: "Password"
+      },
+      submitButton: {
+        text: "Submit"
+      }
+    };
+    options = Object.assign(options, this.props.options || {});
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+      <form>
+            <div>
+              <label>{options.email.label}</label>
+              <input type="email" onChange={this.onChange} placeholder={options.email.placeholder} />
+            </div>
+            <div>
+              <label>{options.password.label}</label>
+              <input type="password" onChange={this.onChange}  placeholder={options.password.placeholder}required={true} minLength={5} />
+
+            </div>
+            <button type="submit" onClick={this.onSubmit}>{options.submitButton.text}</button>
+          </form>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
