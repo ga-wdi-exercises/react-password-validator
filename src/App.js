2e46b86325d8774907bd68111b1a69edb2b06049
Ba Van Phan <bavphan@gmail.com>
2016-12-12 11:46:33 -0500

    finished password validator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      validation: ''
+    }
+  }
+password(e){
+  this.setState({password: e.target.value})
+}
+validation(e){
+  this.setState({validation: e.target.value})
+}
+  validatePassword(){
+    console.log(this.state.password)
+    if (this.state.password === this.state.validation){
+      alert("holy balls you did it")
+    }else {
+      alert("nope youre a failure")
+    }
+  }
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Password Validator 9000</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <ul>
+          <li>
+          <label>
+            Name:
+            <input type="text"/>
+          </label>
+          </li>
+          <li>
+          <label>
+            Password:
+            <input type="password" value={this.state.password} onChange={ e => this.password(e) }/>
+          </label>
+          </li>
+          <li>
+          <label>
+            Confirm Password:
+            <input type="password" value={this.state.validation} onChange={ e => this.validation(e) }/>
+          </label>
+          </li>
+          <button value="Submit" onClick={() => {this.validatePassword()}}>SUMBIT DIS JUNX</button>
+        </ul>
+          </form>
       </div>
     );
   }
