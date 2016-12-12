5872e6b2469f00eee46172ad228dbff83f2f93ac
Thinh Nguyen <nguyendthinh@outlook.com>
2016-12-12 11:23:03 -0500

    completed lab

--- src/App.js
+++ src/App.js
@@ -1,21 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class User extends Component {
+
+  constructor(props){
+    super()
+    this.state = {
+      email: "",
+      password: "",
+      confirmation: ""
+    }
+  }
+
+////
+
+  validate(e) {
+    e.preventDefault()
+
+    this.setState({
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value,
+      confirmation: document.getElementById("confirmation").value
+    },
+    function validation() {
+
+      if ((this.state.email && this.state.password && this.state.confirmation) !== "") {
+          console.log(this.state.email);
+          console.log(this.state.password);
+          console.log(this.state.confirmation);
+
+        if (this.state.password === this.state.confirmation) {
+          alert("You have successfully signed up!")
+        } else {
+          alert("Please reconfirm your passwords. The values are not the same for the last two fields")
+        }
+      }
+
+      else {
+        alert("Please filled all required fields.")
+      }
+    }
+  )
+
+  }
+
+//////
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <h1>User Sign Up</h1>
+
+        <form onSubmit={(e) => this.validate(e)}>
+        <p>Email: <input id="email" type="text" /></p>
+        <p>Password: <input id="password" type="text" /></p>
+        <p>Confirm Password: <input id="confirmation" type="text" /></p>
+        <p><input type="submit" /></p>
+        </form>
+
       </div>
     );
   }
 }
 
-export default App;
+export default User;
