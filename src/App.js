378bacd29f5b5e063b0b65b7490c706df7c376c8
amlak befekadu <amlakgirma@yahoo.com>
2017-05-26 11:15:58 -0400

    validation id works good

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Validator from './Validator';
+import Input from './Input'
 
 class App extends Component {
   render() {
     return (
-      <Validator />
+      <Input />
     );
   }
 }
