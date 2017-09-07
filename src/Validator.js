096a0326b8cead4d5a700727869f144db1fa5519
Frew A Negash <zulufanke@yahoo.com>
2017-09-06 21:12:40 -0400

    react-password-validator lab work submitted

--- src/Validator.js
+++ src/Validator.js
@@ -2,14 +2,44 @@ import React, { Component } from 'react';
 import "./Validator.css";
 
 class Validator extends Component {
+  constructor (props){
+    super(props)
+    this.state= {
+      email: null,
+      password: null,
+      confirmPassword: null
+    }
+  }
+
+  imputpassword(e) {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  imputconfirmPassword(e) {
+    this.setState({
+      confirmPassword: e.target.value
+    })
+  }
+
+
   render() {
+    let passwordResult
+    if (this.state.password===this.state.confirmPassword){
+        passwordResult="valid"
+    }
+    else {
+        passwordResult="Wrong password"
+    }
     return (
       <div className="form">
         <h1>Sign Up</h1>
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <input type="password" placeholder="confirm password" />
-        <input type="submit" value="Submit" />
+        <input type="text" placeholder="email" onChange={(e) => this.imputEmail(e)}/>
+        <input type="password" placeholder="password" onChange={(e) => this.imputpassword(e)}/>
+        <input type="password" placeholder="confirm password" onChange={(e) => this.imputconfirmPassword(e)}/>
+        <input type="submit" value="Submit"/>
+        <p>{passwordResult}</p>
       </div>
     );
   }
